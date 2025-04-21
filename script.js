e49d36559e46bdf4cda6107dfe454358114884b5
unknown <thegrandgemini@gmail.com>
2025-04-21 01:56:00 +0100

    Improved layout and responsiveness across all devices

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
-document.addEventListener("DOMContentLoaded", () => {
-        const targets = document.querySelectorAll(".hero-content, .hero-form");
+'use strict';
 
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add("visible");
-                    observer.unobserve(entry.target); // animate only once
-                }
-            });
-        }, {
-            threshold: 0.1
-        });
+function toggleMenu() {
+	const menu = document.querySelector('.mobile-nav-links');
+	menu.classList.toggle('list-active');
+	console.log('menu clicked');
+}
 
-        targets.forEach(target => {
-            observer.observe(target);
-        });
-    });    
-    const targets = document.querySelectorAll(".product-box, .product-box img");
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("visible");
-                observer.unobserve(onscroll.target); // animate only once
-            }
-        });
-    }, {
-        threshold:0.1,
-    });
-    targets.forEach(target => {
-        observer.observe(target);
-    });
+document.addEventListener('click', function (event) {
+	const menu = document.querySelector('.mobile-nav-links');
+	const sandwich = document.querySelector('.sandwich-bar');
+
+	if (!menu.contains(event.target) && !sandwich.contains(event.target)) {
+		menu.classList.remove('list-active');
+	}
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	const targets = document.querySelectorAll('.hero-content, .hero-form');
+
+	const observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add('visible');
+					observer.unobserve(entry.target); // animate only once
+				}
+			});
+		},
+		{
+			threshold: 0.1,
+		}
+	);
+
+	targets.forEach((target) => {
+		observer.observe(target);
+	});
+});
+const targets = document.querySelectorAll('.product-box, .product-box img');
+const observer = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('visible');
+				observer.unobserve(onscroll.target); // animate only once
+			}
+		});
+	},
+	{
+		threshold: 0.1,
+	}
+);
+targets.forEach((target) => {
+	observer.observe(target);
+});
